b640e3e628fa10e89ef22a6954e7b544c47bbb79
Felipe D'Alessio <felipe.dalessio123@gmail.com>
2018-05-09 21:55:10 -0300

    Atualização do codigo.

--- src/main/java/com/example/list/NewList.java
+++ src/main/java/com/example/list/NewList.java
@@ -2,15 +2,20 @@ package com.example.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.example.list.Car;
+import com.google.common.collect.Collections2;
 
 public class NewList {
 
 	public static void main(String[] args) {
-		// Essa instância deveria ser criada fora do main, e utilizando o autowired do spring, como uma bean
+		// Essa instancia deveria ser criada fora do main, e utilizando o autowired do spring, como uma bean
 		NewList a = new NewList();
 		a.combineLists()
 			.stream()
@@ -19,27 +24,41 @@ public class NewList {
 	}
 
 	public List<Car> combineLists() {
-		// Recebe os items de uma requisição 
+		// Recebe os items de uma requisicao 
 		List<Car> lista1 = new ArrayList<>();
+		List<Car> lista2 = new ArrayList<>();
 		
 		Car civic = new Car.Builder()
 				.withName("civic")
 				.withColor("branco")
 				.withValue(89999.99)
-				.withItems(Arrays.asList("ar-condicionado digital", "banco de couro", "direção elétrica"))
+				.withItems(Arrays.asList("ar-condicionado digital", "banco de couro", "direcao eletrica"))
 				.build();
 		
 		Car fit = new Car.Builder()
 				.withName("fit")
 				.withColor("vermelho")
 				.withValue(59999.99)
-				.withItems(Arrays.asList("ar-condicionado manual", "direção elétrica", "vidros elétricos"))
+				.withItems(Arrays.asList("ar-condicionado manual", "direcao eletrica", "vidros eletricos"))
+				.build();
+		
+		Car hrv = new Car.Builder()
+				.withName("fit")
+				.withColor("vermelho")
+				.withValue(109999.99)
+				.withItems(Arrays.asList("ar-condicionado digital", "suv"))
 				.build();
 
 		lista1.add(civic);
 		lista1.add(fit);
+		lista1.add(hrv);
 		
-		// Começa a tratar para retornar
+		lista2.add(civic);
+		lista2.add(fit);
+		
+		
+		System.out.println();
+		// Comeca a tratar para retornar
 		return lista1.stream()
 				.map(a -> discount(a))
 				.collect(Collectors.toList());
